d6a01c01cd17854d4da76b9277797cbb9fa81b0c
kot lena <aternetas@gmail.com>
2023-04-13 12:04:55 +0300

    made a separate class Contract for activities

--- app/src/main/java/com/example/navigation/activities/GameActivity.kt
+++ app/src/main/java/com/example/navigation/activities/GameActivity.kt
@@ -1,10 +1,7 @@
 package com.example.navigation.activities
 
-import android.content.Context
-import android.content.Intent
 import android.os.Build
 import android.os.Bundle
-import androidx.activity.result.contract.ActivityResultContract
 import com.example.navigation.databinding.ActivityGameBinding
 import com.example.navigation.models.Options
 
@@ -22,22 +19,8 @@ class GameActivity : BaseActivity() {
         }
     }
 
-    class Contract : ActivityResultContract<Options, Options>() {
-        override fun createIntent(context: Context, input: Options) =
-            Intent(context, GameActivity::class.java).apply {
-                putExtra(EXTRA_INPUT_OPTIONS, input)
-            }
-
-        override fun parseResult(resultCode: Int, intent: Intent?) =
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                intent?.getParcelableExtra(EXTRA_OUTPUT_OPTIONS, Options::class.java) ?: Options.DEFAULT
-            } else {
-                intent?.getParcelableExtra(EXTRA_OUTPUT_OPTIONS) ?: Options.DEFAULT
-            }
-    }
-
     companion object {
-        private const val EXTRA_INPUT_OPTIONS = "EXTRA_OPTIONS"
-        private const val EXTRA_OUTPUT_OPTIONS = "EXTRA_OPTIONS"
+        const val EXTRA_INPUT_OPTIONS = "EXTRA_OPTIONS"
+        const val EXTRA_OUTPUT_OPTIONS = "EXTRA_OPTIONS"
     }
 }
